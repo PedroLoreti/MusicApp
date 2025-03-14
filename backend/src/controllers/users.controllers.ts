6f66953d09eb12dd1e0dedf18e3943774992af40
Loreti <111466632+PedroLoreti@users.noreply.github.com>
2025-03-13 22:53:34 -0300

    update musicApp

--- backend/src/controllers/users.controllers.ts
+++ backend/src/controllers/users.controllers.ts
@@ -2,13 +2,12 @@ import { UserServices } from "../services/users.services";
 import { Request, Response } from "express";
 
 export class UsersControllers {
-    
 
     async createUser(req: Request, res: Response) {
         const userServices = new UserServices();
 
         const response = await userServices.createUser(req.body);
 
-        return res.status(201).json(response);
+        res.status(201).json(response);
     }
 }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { UsersControllers } from "../controllers/users.controllers";
+import { IsValidBody } from "../middlewares/validationMiddleware.middleware";
+import { createUserSchema } from "../schemas/userSchema.schema";
 
 export const userRoutes = Router();
 
 
 const usersControllers = new UsersControllers();
 
-userRoutes.post("/", )
+userRoutes.post("/", IsValidBody.execute({ body: createUserSchema }), usersControllers.createUser);
